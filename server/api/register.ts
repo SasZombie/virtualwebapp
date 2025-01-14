e70932d0e4fa8d2b34ce6d9f51070f1465d2c29a
SasZombie <gigelusas81@gmail.com>
2025-01-14 10:48:33 +0200

    Email added

--- server/api/register.ts
+++ server/api/register.ts
@@ -1,7 +1,9 @@
 import { defineEventHandler, readBody } from "h3";
 import { hashPassword } from "@/typscript/utils";
+import { GmailAddr, GmailPass } from "@/server/secret"
 
 import { FormBody, redis } from "./common";
+import nodemailer from 'nodemailer';
 
 function randomID(
   length: number,
@@ -17,6 +19,30 @@ function randomID(
   }
 
   return result;
+}
+
+async function sendEmail(to: string, subject: string, text: string) {
+  const transporter = nodemailer.createTransport({
+      service: 'gmail', 
+      auth: {
+          user: GmailAddr,       
+          pass: GmailPass,       
+      },
+  });
+
+  const mailOptions = {
+      from: '"CompanyIRL" <your_email@gmail.com>', 
+      to,                                         
+      subject,                                    
+      text,                                       
+  };
+
+  try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Message sent: %s', info.messageId);
+  } catch (error) {
+      console.error('Error sending email:', error);
+  }
 }
 
 export default defineEventHandler(async (event) => {
@@ -39,10 +65,13 @@ export default defineEventHandler(async (event) => {
   }
 
   try {
+
     const hashedPassword = hashPassword(password);
     const virtualMachineNumber = 0
     const id = name + randomID(4) + ":virtualMachines";
     await redis.hmset(key, { name, hashedPassword, virtualMachineNumber, id });
+    await sendEmail(email, 'Confirmation email', 'Confirmation email .');
+
     setResponseStatus(event, 201, "Registration complete!");
     return;
   } catch (error) {
---
+++ server/secret.ts
@@ -0,0 +1,4 @@
+const GmailAddr = "e8899218@gmail.com"
+const GmailPass = "gdhk xqbw evjq tlmr"
+
+export {GmailAddr, GmailPass}
